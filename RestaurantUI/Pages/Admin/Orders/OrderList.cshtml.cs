4e26b04b4c5246a0751f1c826259414ccbaf3eac
MOHAMED EL JIHAOUI <eljihaoui@gmail.com>
2022-09-09 08:10:43 +0100

    104_Add Status buttons filter in Order List

--- RestaurantUI/Pages/Admin/Orders/OrderList.cshtml.cs
+++ RestaurantUI/Pages/Admin/Orders/OrderList.cshtml.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Restaurant.DAL.Interfaces;
 using Restaurant.Models;
+using Restaurant.Utility;
 
 namespace RestaurantUI.Pages.Admin.Orders
 {
- 
+
     public class OrderListModel : PageModel
     {
         private readonly IUnitOfWork _unitofWork;
@@ -17,10 +18,29 @@ namespace RestaurantUI.Pages.Admin.Orders
         public void OnGet()
         {
         }
-        public async Task<IActionResult> OnGetList()
+        public async Task<IActionResult> OnGetList(string? status = null)
         {
-            OrderList =await _unitofWork.OrderRepo.GetAll(includeProperties: "ApplicationUser");
+            OrderList = await _unitofWork.OrderRepo.GetAll(includeProperties: "ApplicationUser");
+            switch (status)
+            {
+                case "cancelled":
+                    OrderList = OrderList.Where(o => o.Status == ConstDefs.StatusCancelled
+                    || o.Status == ConstDefs.StatusRejected);
+                    break;
+                case "completed":
+                    OrderList = OrderList.Where(o => o.Status == ConstDefs.StatusCompleted);
+                    break;
+                case "ready":
+                    OrderList = OrderList.Where(o => o.Status == ConstDefs.StatusReady);
+                    break;
+                case "inprocess":
+                    OrderList = OrderList.Where(o => o.Status == ConstDefs.StatusInProcess);
+                    break;
+                default:
+                    OrderList = OrderList;
+                    break;
+            }
             return new JsonResult(new { data = OrderList });
-       }
+        }
     }
 }
